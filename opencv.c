cad94bee3f255c0225965f032a420ffa989afcba
jackbackrack <jrb@pobox.com>
2017-08-25 21:35:13 -0700

    add grab frame and dumping

--- opencv.c
+++ opencv.c
@@ -47,3 +47,69 @@ IplImage* cv_create_image (int* size, int depth, int columns) {
   return res;
 }
 
+static void cv_grab_frame (IplImage *image) {
+  GLint view_port[4];
+  GLint last_buffer;
+  glGetIntegerv(GL_VIEWPORT, view_port);
+  glPixelStorei(GL_PACK_ALIGNMENT, 1);
+  glPixelStorei(GL_PACK_ROW_LENGTH, 0);
+  glPixelStorei(GL_PACK_SKIP_ROWS, 0);
+  glPixelStorei(GL_PACK_SKIP_PIXELS, 0);
+  glGetIntegerv(GL_READ_BUFFER, &last_buffer);
+  glReadBuffer(GL_FRONT);
+  glReadPixels
+    (0, 0, view_port[2], view_port[3], 
+     GL_BGR_EXT, GL_UNSIGNED_BYTE, image->imageData);
+  glReadBuffer(last_buffer);
+  cvFlip(image, image, 0);
+}
+
+#include <time.h>
+
+IplImage *pic_image;
+
+void cv_dump_viewport (void) {
+  if (!pic_image) {
+    GLint view_port[4];
+    glGetIntegerv(GL_VIEWPORT, view_port);
+    CvSize sz = cvSize(view_port[2], view_port[3]);
+    pic_image = cvCreateImage( sz, 8, 3 );
+  }
+  cv_grab_frame(pic_image);
+  char filename[256];
+  time_t rawtime;
+  struct tm * timeinfo;
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+  strftime(filename,sizeof(filename),"./dump/%Y-%m-%d-%H-%M-%S-pic.jpg", timeinfo);
+  int res = cvSaveImage(filename, pic_image, 0);
+}
+
+/*
+CvVideoWriter* dumper  = 0;
+int fourcc = -1;
+IplImage *dump_image;
+// int fourcc = CV_FOURCC('A', 'V', 'C', '1'); // h.264
+// int fourcc = CV_FOURCC('D','I','V','X'); // mp4
+// int fourcc = CV_FOURCC('H','F','Y','U');
+
+void cv_write_avi_out (void) {
+  if (num_frames % speed == 0) {
+    grab_frame(dump_image);
+    if (is_dumping_avi) {
+      post("WRITING FRAME %d\n", num_dumped_frames);
+      cvWriteToAVI(dumper, dump_image);
+    } else if (is_dumping_jpgs) {
+      char filename[256];
+      sprintf(filename, "%s%04d.jpg", dump_filename, num_dumped_frames);
+      int res = cvSaveImage(filename, dump_image, 0);
+    }
+    num_dumped_frames += 1;
+  }
+  num_frames += 1;
+  if (num_frames > 0 && (num_frames % (int)(num_data(target_fps)*speed)) == 0) {
+    int secs = num_frames / (num_data(target_fps)*speed);
+    post("T=%02d:%02d\n", secs / 60, secs % 60);
+  }
+}
+*/
